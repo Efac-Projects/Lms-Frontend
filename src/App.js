d41430af1e2e8bf5636f509dc9ef5c2a87ae368d
Thilina <hellowthilinafive@gmail.com>
2022-06-02 23:26:34 +0530

    add working database table to the front end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import studentService from './services/student.service';
 
 const App = () => {
   const [students, setStudent] = useState([]);
 
+  useEffect(() => {
+    studentService
+      .getAll()
+      .then((response) => {
+        console.log('printing students', response.data);
+        setStudent(response.data);
+      })
+      .catch((error) => {
+        console.log('There is error', error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <h3>Student Database</h3>
+      <div>
+        <table border='1' cellPadding='10'>
+          <tr>
+            <td>Name</td>
+            <td>Department</td>
+            <td>Email</td>
+          </tr>
+          {students.map((student) => {
+            <tr key={student.id}>
+              <td>{student.name}</td>
+              <td>{student.department}</td>
+              <td>{student.email}</td>
+            </tr>;
+          })}
+        </table>
+      </div>
     </div>
   );
 };
